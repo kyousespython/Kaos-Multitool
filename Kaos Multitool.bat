80e8b5ed0f19662b13772f2bd039800ea381f7da
kaos <106881639+kyousespython@users.noreply.github.com>
2022-08-01 20:13:41 +0200

    Add files via upload

---
+++ Kaos Multitool.bat
@@ -0,0 +1,262 @@
+@echo off
+mode con lines=45 cols=95
+chcp 65001 >nul
+:again
+cls
+color 9
+title Kaos Multi Tool -- kyo#2222
+:rainbow
+echo.
+echo ██╗  ██╗ █████╗  ██████╗ ███████╗    ███╗   ███╗██╗   ██╗██╗  ████████╗██╗████████╗ ██████╗  ██████╗ ██╗     
+echo ██║ ██╔╝██╔══██╗██╔═══██╗██╔════╝    ████╗ ████║██║   ██║██║  ╚══██╔══╝██║╚══██╔══╝██╔═══██╗██╔═══██╗██║     
+echo █████╔╝ ███████║██║   ██║███████╗    ██╔████╔██║██║   ██║██║     ██║   ██║   ██║   ██║   ██║██║   ██║██║     
+echo ██╔═██╗ ██╔══██║██║   ██║╚════██║    ██║╚██╔╝██║██║   ██║██║     ██║   ██║   ██║   ██║   ██║██║   ██║██║     
+echo ██║  ██╗██║  ██║╚██████╔╝███████║    ██║ ╚═╝ ██║╚██████╔╝███████╗██║   ██║   ██║   ╚██████╔╝╚██████╔╝███████╗
+echo ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝    ╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝   ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝                                                                                                                                                                                                                                                                
+echo.
+echo ---------------------------------------------------------------
+echo.
+echo Welcome to the main menu.
+echo.
+echo ---------------------------------------------------------------
+echo .
+echo coded by kyo#2222 
+echo.
+echo  1 - putty
+echo  2 - google
+echo  3 - pinger 
+echo  4 - iplookup_gui
+echo  5 - iplookup_cli
+echo  6 - pScan
+echo  7 - Dox Tool
+echo  8 - Discord Username To IP
+echo  9 - DDOS Tools
+echo  10 - Connected Devices
+echo  11 - PCPS
+echo  12 - JunkWare Removal Tool
+echo  13 - Rouge Killer Malware Removal Tool
+echo  14 - Page 2
+echo.
+:mainlogo
+echo.
+set /p main=Choose your tool:
+if %main%== 1 goto putty
+if %main%== 2 goto google
+if %main%== 3 goto pinger
+if %main%== 4 goto iplookup_gui
+if %main%== 5 goto iplookup_cli
+if %main%== 6 goto pScan
+if %main%== 7 goto Dox Tool
+if %main%== 8 goto disc
+if %main%== 9 goto ddos
+if %main%== 10 goto Cndv
+if %main%== 11 goto PCPS
+if %main%== 12 goto jrt
+if %main%== 13 goto rgk
+if %main%== 14 goto main2
+goto mainlogo
+timeout >2
+goto mainlogo
+echo Wrong Number
+:pinger
+start pinger.bat
+goto main
+:google
+start https://google.com
+goto main
+:putty
+start putty.exe
+goto main
+:iplookup_gui
+start iplookup.exe
+goto main
+:iplookup_cli
+start iplookup.bat
+goto main
+:pScan
+start pScan.exe
+goto main
+:Dox Tool
+start DoxRapist.exe
+goto main
+:disc
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+goto main
+:ddos
+start ddostools.bat
+goto main
+:PCPS
+start PCPS.exe
+goto main
+:jrt
+start JRT.exe
+goto main
+:rgk
+start rgk.exe
+goto main
+:Cndv
+start iplog.bat
+goto main
+:main2
+cls
+echo Welcome %USERNAME%
+echo.
+echo  ██████╗  ██████╗ ██╗  ████████╗██╗   ██╗    ████████╗ ██████╗  ██████╗ ██╗     ███████╗
+echo  ██╔══██╗██╔═══██╗██║  ╚══██╔══╝╚██╗ ██╔╝    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝
+echo  ██████╔╝██║   ██║██║     ██║    ╚████╔╝        ██║   ██║   ██║██║   ██║██║     ███████╗
+echo  ██╔══██╗██║   ██║██║     ██║     ╚██╔╝         ██║   ██║   ██║██║   ██║██║     ╚════██║
+echo  ██████╔╝╚██████╔╝███████╗██║      ██║          ██║   ╚██████╔╝╚██████╔╝███████╗███████║
+echo  ╚═════╝  ╚═════╝ ╚══════╝╚═╝      ╚═╝          ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝                                                                                                                                                      
+echo.
+echo Second page of the main menu.
+echo.
+echo  1 - Back To Page 1
+echo  2 - wannabe1337
+echo  3 - webresolver.nl 
+echo  4 - vedbex.com
+echo  5 - thatsthem.com
+echo  6 - Hazard
+echo  7 - OSNIT Framework 
+echo  8 - USA Fake Identity Generator
+echo  9 - Mac Spoofer
+echo 10 - Ping Of Death
+echo 11 - Illegal Services
+echo 12 - Scammer Toolbox (Takes a few seconds to load) 
+echo 13 - OpenBullet (Broken for some people) 
+echo 14 - Page 3
+echo.
+:mainlogo2
+echo.
+set /p main=Choose your tool:
+if %main%== 1 goto main
+if %main%== 2 goto wana
+if %main%== 3 goto webr
+if %main%== 4 goto ved
+if %main%== 5 goto ttcom
+if %main%== 6 goto haz
+if %main%== 7 goto osnit
+if %main%== 8 goto ufig
+if %main%== 9 goto macspf
+if %main%== 10 goto pingod
+if %main%== 11 goto ileser
+if %main%== 12 goto scambox
+if %main%== 13 goto ob
+if %main%== 14 goto main3
+goto mainlogo2
+timeout >2
+goto mainlogo2
+echo Wrong Number
+:haz
+start hazard.exe
+goto main2
+:wana
+start https://wannabe1337.xyz
+goto main2
+:webr
+start https://webresolver.nl
+goto main2
+:ved
+start https://vedbex.com
+goto main2
+:ttcom
+start https://thatsthem.com
+goto main2
+:osnit
+start https://osintframework.com
+goto main2
+:ufig
+start https://www.fakenamegenerator.com
+goto main2
+:macspf
+start batchspf
+goto main2
+:pingod
+start PingOfDeath.bat
+goto main2
+:ileser
+start ileserv
+goto main2
+:scambox
+start scambox
+goto main2
+:ob
+start OpenBullet
+goto main2
+
+:main3
+cls
+echo Welcome %USERNAME%
+echo.
+echo  ██████╗  ██████╗ ██╗  ████████╗██╗   ██╗    ████████╗ ██████╗  ██████╗ ██╗     ███████╗
+echo  ██╔══██╗██╔═══██╗██║  ╚══██╔══╝╚██╗ ██╔╝    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝
+echo  ██████╔╝██║   ██║██║     ██║    ╚████╔╝        ██║   ██║   ██║██║   ██║██║     ███████╗
+echo  ██╔══██╗██║   ██║██║     ██║     ╚██╔╝         ██║   ██║   ██║██║   ██║██║     ╚════██║
+echo  ██████╔╝╚██████╔╝███████╗██║      ██║          ██║   ╚██████╔╝╚██████╔╝███████╗███████║
+echo  ╚═════╝  ╚═════╝ ╚══════╝╚═╝      ╚═╝          ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝                                                                                                                                                      
+echo.
+echo Third page of the main menu.
+echo.
+echo  1 - Back To Page 1
+echo  2 - Matrix Batch (Opens cmd window with matrix effect)
+echo  3 - PcCleaner
+echo  4 - RarFileBruteforcer
+echo  5 - popup spammer
+echo  6 - Account Generators
+echo  7 - UDP Unicorn (USE VPN)
+echo  8 - Mercurial Token Grabber Builder
+echo  9 - Avast Precracked Download
+echo 10 - 
+echo 11 - 
+echo 12 - 
+echo 13 - 
+echo.
+:mainlogo3
+echo.
+set /p main=Choose your tool:
+if %main%== 1 goto main
+if %main%== 2 goto mbat
+if %main%== 3 goto pcle
+if %main%== 4 goto rfbf
+if %main%== 5 goto ppsr
+if %main%== 6 goto accgens
+if %main%== 7 goto udpuni
+if %main%== 8 goto merigrab
+if %main%== 9 goto avast
+if %main%== 10 goto main3
+if %main%== 11 goto main3
+if %main%== 12 goto main3
+if %main%== 13 goto main3
+if %main%== 14 goto main3
+goto mainlogo2
+timeout >2
+goto mainlogo2
+echo Wrong Number
+:mbat
+start matrix.bat
+goto main3
+:pcle
+start PcCleanUp.bat
+goto main3
+:rfbr
+start craker.bat
+goto main3
+:ppsr
+start spam.vbs
+goto main3
+:accgens
+start generators.bat
+goto main3
+:merigrab
+start https://github.com/NightfallGT/Mercurial-Grabber/releases/download/v1.0/Mercurial.Grabber.v1.03.rar
+goto main3
+:avast 
+start https://icedrive.net/s/b6ZACGBf6YxtCtuNYRyXQBiGASXD
+goto main3
+:udpuni
+start udpuni.exe
+goto main3
+goto rainbow
+
+
+
+
---
+++ Kaos Sites.bat
@@ -0,0 +1,127 @@
+@echo off
+chcp 65001
+:again
+cls
+color 5
+title BOLTY Websites
+:main
+cls
+echo ▄▄▄▄    ▒█████   ██▓  ▄▄▄█████▓▓██   ██▓    █     █░▓█████  ▄▄▄▄     ██████  ██▓▄▄▄█████▓▓█████   ██████ 
+echo ▓█████▄ ▒██▒  ██▒▓██▒  ▓  ██▒ ▓▒ ▒██  ██▒   ▓█░ █ ░█░▓█   ▀ ▓█████▄ ▒██    ▒ ▓██▒▓  ██▒ ▓▒▓█   ▀ ▒██    ▒ 
+echo ▒██▒ ▄██▒██░  ██▒▒██░  ▒ ▓██░ ▒░  ▒██ ██░   ▒█░ █ ░█ ▒███   ▒██▒ ▄██░ ▓██▄   ▒██▒▒ ▓██░ ▒░▒███   ░ ▓██▄   
+echo ▒██░█▀  ▒██   ██░▒██░  ░ ▓██▓ ░   ░ ▐██▓░   ░█░ █ ░█ ▒▓█  ▄ ▒██░█▀    ▒   ██▒░██░░ ▓██▓ ░ ▒▓█  ▄   ▒   ██▒
+echo ░▓█  ▀█▓░ ████▓▒░░██████▒▒██▒ ░   ░ ██▒▓░   ░░██▒██▓ ░▒████▒░▓█  ▀█▓▒██████▒▒░██░  ▒██▒ ░ ░▒████▒▒██████▒▒
+echo ░▒▓███▀▒░ ▒░▒░▒░ ░ ▒░▓  ░▒ ░░      ██▒▒▒    ░ ▓░▒ ▒  ░░ ▒░ ░░▒▓███▀▒▒ ▒▓▒ ▒ ░░▓    ▒ ░░   ░░ ▒░ ░▒ ▒▓▒ ▒ ░
+echo ▒░▒   ░   ░ ▒ ▒░ ░ ░ ▒  ░  ░     ▓██ ░▒░      ▒ ░ ░   ░ ░  ░▒░▒   ░ ░ ░▒  ░ ░ ▒ ░    ░     ░ ░  ░░ ░▒  ░ ░
+echo  ░    ░ ░ ░ ░ ▒    ░ ░   ░       ▒ ▒ ░░       ░   ░     ░    ░    ░ ░  ░  ░   ▒ ░  ░         ░   ░  ░  ░  
+echo  ░          ░ ░      ░  ░        ░ ░            ░       ░  ░ ░            ░   ░              ░  ░      ░  
+echo       ░                          ░ ░                              ░                                       
+echo welcome to BOLTY Websites
+echo.
+echo 1 - fortnite cheat                                                                           
+echo 2 - Reborn Launcher gta 5 cheat
+echo 3 - kiddons menu gta 5 cheat
+echo 4 - syslx menu gta 5 cheat
+echo 5 - galaxy protector a .net obfusticator
+echo 6 - terror mod menu gta 5 cheat
+echo 7 - token checker
+echo 8 - nitro gen
+echo 9 - eternity cheat gta 5 
+echo 10 - crespo cheat gta 5
+echo 11 - slay gta 5 cheat
+echo 12 joker gta 5 cheat 
+echo 13 inertia minecraft cheat
+echo 14 aristois minecraft cheat
+echo 15 wolfram minecraft cheat
+echo 16 liquidbounce minecraft cheat
+echo 17 best free executor krnl
+echo 18 linkvertise bypasser for krnl key
+echo 19 mercurial grabber
+echo 20 itroublvetsc
+echo 21 discord-aio
+echo 22 eclipse grabber
+echo 23 blitzed grabber
+echo 24 kiwee-grabber
+echo 25 viper grabber
+echo 26 depression-logger
+echo 27 hazard token grabber
+echo 28 qvoid token grabber
+echo 29 venaxyt token grabber
+echo 30 force venom grabber
+echo 31 vespy logger
+echo 32 pirate stealer
+echo 33 orion grabber
+echo 34 njrat
+echo 35 njrat danger edition
+echo 36 njrat golden edition
+echo 37 njrat green edition
+echo 38 njrat lime edition
+echo 39 vanilla rat
+echo 40 async rat
+echo 41 888 rat
+echo 42 dc rat
+echo 43 stich rat
+echo 44 oneparsec rat
+echo 45 the fat rat
+echo 46 revenge rat
+echo 47 hazard nuker
+echo 48 gang-nuker
+echo 49 lithium nuker
+echo 50 kyanite nuker
+echo 51 17teen nuker
+echo.
+set /p main=Choose Your Number:
+
+if %main% == 1 start https://cdn.discordapp.com/attachments/977989524770672692/977989552004296774/GorillaHook_FORTNITE_2.rar
+if %main% == 2 start https://cdn.discordapp.com/attachments/977184713510879332/977184910680940614/Reborn_Launcher.exe
+if %main% == 3 start https://cdn.discordapp.com/attachments/977180782122639371/977181937078763540/modest-menu_v0.9.3.zip
+if %main% == 4 start https://cdn.discordapp.com/attachments/977184320722706492/977184520115748884/SYSLX_Free_2.9.5.zip
+if %main% == 5 start https://cdn.discordapp.com/attachments/965215995423784960/965216255193809006/GalaxyProtector.zip
+if %main% == 6 start https://cdn.discordapp.com/attachments/977178745578348584/977180276381847573/Terror_Menu_Installer_by_Ts.zip
+if %main% == 7 start https://cdn.discordapp.com/attachments/962441822020448266/962816134455910420/TokenChecker_V1.5.zip
+if %main% == 8 start https://cdn.discordapp.com/attachments/977229624255864852/979132587060641792/Nitro_Gen.rar
+if %main% == 9 start https://cdn.discordapp.com/attachments/977181182733217852/977181745122250762/Eternity.rar
+if %main% == 10 start https://cdn.discordapp.com/attachments/977182893292019723/977183410768465920/CrespoMenu.rar
+if %main% == 11 start https://cdn.discordapp.com/attachments/977183750653882408/977183925447315476/SlayModMenu.zip
+if %main% == 12 start https://cdn.discordapp.com/attachments/977182276314099712/977182646750834698/Joker_Mod_Menu.rar
+if %main% == 13 start https://cdn.discordapp.com/attachments/977186030425866310/977186292322426961/Inertia_Client_v3.1.3_MC1.14.4-1.16.5.rar
+if %main% == 14 start https://cdn.discordapp.com/attachments/977186557620527145/977186748432007239/Aristois.ZIP
+if %main% == 15 start https://anonfiles.com/Vf7cmaidy1/Wolfram_1.8_-_1.12_rar
+if %main% == 16 start https://cdn.discordapp.com/attachments/977191218012356619/977191350304911400/LiquidBounce1.8.9.rar
+if %main% == 17 start https://krnl.ca/
+if %main% == 18 start https://thebypasser.com/
+if %main% == 19 start https://github.com/NightfallGT/Mercurial-Grabber
+if %main% == 20 start https://github.com/Itroublve/ItroublveTSC
+if %main% == 21 start https://github.com/Nyxonn/Discord-AIO
+if %main% == 22 start https://github.com/3ct0s/eclipse-grabber
+if %main% == 23 start https://cdn.discordapp.com/attachments/977197118152409099/979052978537377873/Blitzed69fix.rar
+if %main% == 24 start https://github.com/vesperlol/Kiwee-Grabber
+if %main% == 25 start https://cdn.discordapp.com/attachments/966297544101625856/966297868036087878/ViperGrabber.rar
+if %main% == 26 start https://mega.nz/file/jNdVgSYY#_PKaQ5gXLn3EHsRL6CO3WJ8Gdgx4uRbIgT3Sz5LJzrA
+if %main% == 27 start https://github.com/Rdimo/Hazard-Token-Grabber
+if %main% == 28 start https://github.com/Enum0x539/Qvoid-Token-Grabber
+if %main% == 29 start https://github.com/venaxyt/Token-Grabber-Advanced
+if %main% == 30 start https://mega.nz/file/iahnFKYa#OTnIWio8ZRIM0eTY1baIOKb-_IsWOh3TuCaRbVZfOJ4
+if %main% == 31 start https://github.com/vesperlol/Vespy-Logger0.3
+if %main% == 32 start https://github.com/Stanley-GF/PirateStealer
+if %main% == 33 start https://cdn.discordapp.com/attachments/977203920596258888/977204510013407262/Orion_Free_v1.1.zip
+if %main% == 34 start https://github.com/oneparsec/njRAT/releases/download/v0.7D/NjRat.0.7D.zip
+if %main% == 35 start https://github.com/oneparsec/njRAT/releases/download/v0.7D/NjRat.0.7D.Danger.Edition.zip
+if %main% == 36 start https://github.com/oneparsec/njRAT/releases/download/v0.7D/NjRat.0.7D.Golden.Edition.zip
+if %main% == 37 start https://github.com/oneparsec/njRAT/releases/download/v0.7D/NjRat.0.7D.Green.Edition.zip
+if %main% == 38 start https://github.com/oneparsec/njRAT/tree/master/njRAT%20Lime%20Edition
+if %main% == 39 start https://github.com/DannyTheSloth/VanillaRAT
+if %main% == 40 start https://github.com/NYAN-x-CAT/AsyncRAT-C-Sharp
+if %main% == 41 start https://cdn.discordapp.com/attachments/932818090998267946/933902636158169098/888RAT_1.1.1.zip
+if %main% == 42 start https://github.com/qwqdanchun/DcRat
+if %main% == 43 start https://github.com/nathanlopez/Stitch
+if %main% == 44 start https://github.com/OneParsec/njRAT/releases
+if %main% == 45 start https://github.com/screetsec/TheFatRat
+if %main% == 46 start https://cdn.discordapp.com/attachments/972197124840230962/972197447906504734/Revenge-RAT_v.0.2.rar
+if %main% == 47 start https://github.com/Rdimo/Hazard-Nuker
+if %main% == 48 start https://github.com/TT-Tutorials/GANG-Nuker
+if %main% == 49 start https://github.com/verlox/Lithium-Nuker-2
+if %main% == 50 start https://github.com/StvnedEagle1337/KyaniteNuker
+if %main% == 51 start https://github.com/17teen/Discord-Presser-Server-Nuker
+
+                
